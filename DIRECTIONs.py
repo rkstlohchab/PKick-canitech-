c9b022986b3fa7fd0e52f097c591364f3857191a
rkstlohchab <rkstlohchab@gmail.com>
2021-08-20 13:19:21 +0530

    added all files

--- DIRECTIONs.py
+++ DIRECTIONs.py
@@ -1,21 +1,18 @@
 directions = ["east", "south","north", "west", "east"]
-original_i = 0
-i_plus = 1
-for i in range(len(directions)-1):
-    if i_plus < 5:
-        if directions[original_i] == 'east' and directions[i_plus] == 'west':
-            directions.pop(original_i)
-            directions.pop(i_plus)
-        elif directions[original_i] == "north" and directions[i_plus] == 'south':
-            directions.pop(original_i)
-            directions.pop(i_plus)
-        elif directions[original_i] == 'west' and directions[i_plus] == 'east':
-            directions.pop(original_i)
-            directions.pop(i_plus)
-        elif directions[original_i] == "south" and directions[i_plus] == 'north':
-            directions.pop(original_i)
-            directions.pop(i_plus)
-        original_i += 1
-        i_plus += 1
-
+new_direc = []
+index = 0
+for i in directions[:-1]:
+    if i == "east" and directions[index + 1] != "west":
+        if i == "east" and directions[index - 1] != "west":
+            new_direc.append(i)
+    elif i == "north" and directions[index + 1] != "south":
+        if i == "north" and directions[index-1] != "south":
+            new_direc.append(i)
+    elif i == "west" and directions[index + 1] != "east":
+        if i == "west" and directions[index - 1] != "east":
+            new_direc.append(i)
+    elif i == "south" and directions[index + 1] != "north":
+        if i == "south" and directions[index-1] != "north":
+            new_direc.append(i)
+    index += 1
 print(new_direc)
