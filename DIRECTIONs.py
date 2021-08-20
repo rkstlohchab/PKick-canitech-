2fcb8d150f635d415bc5cc2bca1d421dd29200b5
rkstlohchab <rkstlohchab@gmail.com>
2021-08-20 10:34:43 +0530

    added all files

--- DIRECTIONs.py
+++ DIRECTIONs.py
@@ -1,1 +1,21 @@
-ld
+directions = ["east", "south","north", "west", "east"]
+original_i = 0
+i_plus = 1
+for i in range(len(directions)-1):
+    if i_plus < 5:
+        if directions[original_i] == 'east' and directions[i_plus] == 'west':
+            directions.pop(original_i)
+            directions.pop(i_plus)
+        elif directions[original_i] == "north" and directions[i_plus] == 'south':
+            directions.pop(original_i)
+            directions.pop(i_plus)
+        elif directions[original_i] == 'west' and directions[i_plus] == 'east':
+            directions.pop(original_i)
+            directions.pop(i_plus)
+        elif directions[original_i] == "south" and directions[i_plus] == 'north':
+            directions.pop(original_i)
+            directions.pop(i_plus)
+        original_i += 1
+        i_plus += 1
+
+print(new_direc)
